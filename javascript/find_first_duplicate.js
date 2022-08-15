fe81a65b3c8de0039ca32e53ae6c8b8109f500c6
Pete Fowler <fowler.pete@gmail.com>
2022-08-15 15:56:54 -0600

    Add solution

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,6 +1,27 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  const set = new Set();
+
+  for (const int of arr) {
+    if (set.has(int)) {
+      return int;
+    }
+
+    set.add(int);
+  }
+
+  return -1;
 }
+
+/*
+PROBLEM: Find the first duplicate in an array. If none, return -1.
+
+APPROACH: No UI. Input is an array of integers. Output is an integer.
+
+For each letter, check with .filter and .length to see if it matches any other in the array
+  if yes, return that number
+  if no, move to next letter
+   
+*/
 
 if (require.main === module) {
   // add your own tests in here
@@ -15,5 +36,3 @@ if (require.main === module) {
 
 module.exports = findFirstDuplicate;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
